54d35b9c15abc5fac8d82d4d961df2113db337b2
Rareș Opăriuc <rares.opariuc@yahoo.com>
2019-05-09 22:32:11 +0300

    Corrected inheritance for models

--- src/main/java/com/raresopariuc/licenta/model/Immobile.java
+++ src/main/java/com/raresopariuc/licenta/model/Immobile.java
@@ -1,26 +1,18 @@
 package com.raresopariuc.licenta.model;
 
 import com.raresopariuc.licenta.model.audit.UserDateAudit;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
-import javax.validation.constraints.Size;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.*;
 
+@MappedSuperclass
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public abstract class Immobile extends UserDateAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
